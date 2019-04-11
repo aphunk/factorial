cba450f1c7406215e95d5a1baa1d2ac2852f3944
Amy Phung <phung.amy85@gmail.com>
2019-04-10 21:52:39 -0700

    created factorial method

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,6 +1,17 @@
 # Computes factorial of the input number and returns it
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n), where n is the number passed in
+# Space complexity: O(1), constant because no additional space is needed
+
 def factorial(number)
-  raise NotImplementedError
+  if number == nil || number < 0
+    raise ArgumentError, "Enter a positive integer"
+  end
+  
+  factorial = 1
+  while number > 1
+    factorial *= number
+    number -= 1
+  end
+
+  return factorial 
 end
